2f6bd6213b24c4d81814786b6e87ea43e0597a01
Gerson Minichiello <gerson.minichiello>
2024-05-19 13:23:37 -0300

    Initial commit

---
+++ Models/EnumStatusTarefa.cs
@@ -0,0 +1,12 @@
+namespace TrilhaApiDesafio.Models
+{
+	/// <summary>
+	/// Enumarador contendo o status da tarefa. Pendente ou finalizado.
+	/// </summary>
+	public enum EnumStatusTarefa
+	{
+		Pendente,
+		Andamento,
+		Finalizado
+	}
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrilhaApiDesafio.Models
+{
+	/// <summary>
+	/// Representa uma tarefa
+	/// </summary>
+	public class Tarefa
+	{
+		public int Id { get; set; }
+		public string Titulo { get; set; }
+		public string Descricao { get; set; }
+		public DateTime Data { get; set; }
+		public EnumStatusTarefa Status { get; set; }
+	}
+}
